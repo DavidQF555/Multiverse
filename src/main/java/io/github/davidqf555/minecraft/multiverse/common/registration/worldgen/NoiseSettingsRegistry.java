ef9b789da8f3a9ccf8b1b29b33927eae721073f2
DavidQF555 <davidfeng2003@gmail.com>
2023-07-01 01:06:32 -0700

    reverted router changes

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/registration/worldgen/NoiseSettingsRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/registration/worldgen/NoiseSettingsRegistry.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 import java.util.Map;
+import java.util.function.Function;
 
 @Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class NoiseSettingsRegistry {
@@ -27,18 +28,18 @@ public final class NoiseSettingsRegistry {
 
     static {
         ImmutableMap.Builder<ResourceKey<NoiseGeneratorSettings>, NoiseGeneratorSettings> builder = ImmutableMap.builder();
-        builder.put(TOP, createDefaultSettings(false, true, NoiseSettings.create(0, 128, new NoiseSamplingSettings(1, 1, 80, 30), new NoiseSlider(120, 3, 0), new NoiseSlider(-30, 7, 1), 1, 2, TerrainProvider.nether()), 0));
-        builder.put(BOTTOM, createDefaultSettings(true, false, NoiseSettings.create(0, 256, new NoiseSamplingSettings(1, 2, 80, 160), new NoiseSlider(-10, 3, 0), new NoiseSlider(-30, 0, 0), 1, 2, TerrainProvider.overworld(false)), 63));
-        builder.put(NONE, createDefaultSettings(false, false, NoiseSettings.create(0, 128, new NoiseSamplingSettings(2, 1, 80, 160), new NoiseSlider(-3000, 64, -46), new NoiseSlider(-30, 7, 1), 2, 1, TerrainProvider.end()), 0));
-        builder.put(BOTH, createDefaultSettings(true, true, NoiseSettings.create(0, 128, new NoiseSamplingSettings(1, 3, 80, 60), new NoiseSlider(120, 3, 0), new NoiseSlider(320, 4, -1), 1, 2, TerrainProvider.nether()), 32));
+        builder.put(TOP, createDefaultSettings(false, true, NoiseSettings.create(0, 128, new NoiseSamplingSettings(1, 1, 80, 30), new NoiseSlider(120, 3, 0), new NoiseSlider(-30, 7, 1), 1, 2, TerrainProvider.nether()), 0, NoiseRouterData::nether));
+        builder.put(BOTTOM, createDefaultSettings(true, false, NoiseSettings.create(0, 256, new NoiseSamplingSettings(1, 2, 80, 160), new NoiseSlider(-10, 3, 0), new NoiseSlider(-30, 0, 0), 1, 2, TerrainProvider.overworld(false)), 63, noise -> NoiseRouterData.overworldWithNewCaves(noise, false)));
+        builder.put(NONE, createDefaultSettings(false, false, NoiseSettings.create(0, 128, new NoiseSamplingSettings(2, 1, 80, 160), new NoiseSlider(-3000, 64, -46), new NoiseSlider(-30, 7, 1), 2, 1, TerrainProvider.end()), 0, NoiseRouterData::end));
+        builder.put(BOTH, createDefaultSettings(true, true, NoiseSettings.create(0, 128, new NoiseSamplingSettings(1, 3, 80, 60), new NoiseSlider(120, 3, 0), new NoiseSlider(320, 4, -1), 1, 2, TerrainProvider.nether()), 32, NoiseRouterData::nether));
         ALL = builder.build();
     }
 
     private NoiseSettingsRegistry() {
     }
 
-    private static NoiseGeneratorSettings createDefaultSettings(boolean floor, boolean ceiling, NoiseSettings noise, int sea) {
-        return new NoiseGeneratorSettings(noise, Blocks.STONE.defaultBlockState(), Blocks.WATER.defaultBlockState(), NoiseRouterData.overworldWithNewCaves(noise, false), getDefaultSurfaceRule(floor, ceiling), sea, false, true, true, false);
+    private static NoiseGeneratorSettings createDefaultSettings(boolean floor, boolean ceiling, NoiseSettings noise, int sea, Function<NoiseSettings, NoiseRouterWithOnlyNoises> router) {
+        return new NoiseGeneratorSettings(noise, Blocks.STONE.defaultBlockState(), Blocks.WATER.defaultBlockState(), router.apply(noise), getDefaultSurfaceRule(floor, ceiling), sea, false, true, true, false);
     }
 
     private static SurfaceRules.RuleSource getDefaultSurfaceRule(boolean floor, boolean ceiling) {
