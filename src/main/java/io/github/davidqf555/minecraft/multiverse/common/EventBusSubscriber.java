483bf03c5003fe7bcc1e05ba48184128f34a4020
DavidQF555 <davidfeng2003@gmail.com>
2023-06-30 21:19:54 -0700

    removed pointless custom chunk generator

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/EventBusSubscriber.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/EventBusSubscriber.java
@@ -1,44 +1,20 @@
 package io.github.davidqf555.minecraft.multiverse.common;
 
 import io.github.davidqf555.minecraft.multiverse.common.registration.worldgen.FeatureRegistry;
-import io.github.davidqf555.minecraft.multiverse.common.world.gen.DynamicDefaultChunkGenerator;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+@Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public final class EventBusSubscriber {
 
     private EventBusSubscriber() {
     }
 
-    @Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-    public static final class ForgeBus {
-
-        private ForgeBus() {
-        }
-
-        @SubscribeEvent
-        public static void onBiomeLoading(BiomeLoadingEvent event) {
-            event.getGeneration().addFeature(GenerationStep.Decoration.LOCAL_MODIFICATIONS, FeatureRegistry.PLACED_RIFT);
-        }
-
+    @SubscribeEvent
+    public static void onBiomeLoading(BiomeLoadingEvent event) {
+        event.getGeneration().addFeature(GenerationStep.Decoration.LOCAL_MODIFICATIONS, FeatureRegistry.PLACED_RIFT);
     }
 
-    @Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static final class ModBus {
-
-        private ModBus() {
-        }
-
-        @SubscribeEvent
-        public static void onFMLCommonSetup(FMLCommonSetupEvent event) {
-            event.enqueueWork(() -> {
-                Registry.register(Registry.CHUNK_GENERATOR, new ResourceLocation(Multiverse.MOD_ID, "dynamic_default"), DynamicDefaultChunkGenerator.CODEC);
-            });
-        }
-    }
 }
