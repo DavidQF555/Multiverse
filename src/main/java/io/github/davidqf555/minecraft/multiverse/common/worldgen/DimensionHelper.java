8cacdcc424bc0684fadd6bd47f9c10260fbd88eb
DavidQF555 <davidfeng2003@gmail.com>
2023-07-12 00:40:32 -0700

    fixed client biome provider parsing

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/DimensionHelper.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/DimensionHelper.java
@@ -101,13 +101,12 @@ public final class DimensionHelper {
         Holder<NoiseGeneratorSettings> settings = server.registryAccess().registryOrThrow(Registry.NOISE_GENERATOR_SETTINGS_REGISTRY).getHolderOrThrow(type.getNoiseSettingsKey(biomeType));
         float lighting = ceiling ? random.nextFloat() * 0.5f + 0.1f : random.nextFloat() * 0.2f;
         OptionalLong time = ceiling ? OptionalLong.of(18000) : randomTime(random);
-        Registry<Biome> lookup = server.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY);
         BiomeSource provider = new MultiNoiseBiomeSource.Preset(getRegistryKey(index).location(), registry -> new Climate.ParameterList<>(biomes.stream()
-                .map(lookup::getOrCreateHolder)
+                .map(registry::getOrCreateHolder)
                 .map(holder -> {
                     Biome biome = holder.value();
                     return Pair.of(Climate.parameters(biome.getBaseTemperature(), biome.getDownfall(), 0, 0, 0, 0, 0), holder);
-                }).collect(Collectors.toList()))).biomeSource(lookup);
+                }).collect(Collectors.toList()))).biomeSource(server.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY));
         ResourceLocation effect = randomEffect(time.isPresent() && time.getAsLong() < 22300 && time.getAsLong() > 13188, random);
         Holder<DimensionType> dimType = createDimensionType(biomeType == null ? MultiverseType.OVERWORLD.getInfiniburn() : biomeType.getInfiniburn(), type.getHeight(), type.getMinY(), ceiling, time, effect, lighting);
         ChunkGenerator generator;
