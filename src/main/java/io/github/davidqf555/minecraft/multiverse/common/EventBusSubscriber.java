dab79891587318190b467ebafad5f46888ff2e03
DavidQF555 <davidfeng2003@gmail.com>
2023-07-16 18:19:42 -0700

    fixed mixins

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/EventBusSubscriber.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/EventBusSubscriber.java
@@ -1,5 +1,13 @@
 package io.github.davidqf555.minecraft.multiverse.common;
 
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.IMultiverseNoiseGeneratorSettings;
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.MultiverseShape;
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.MultiverseType;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -7,17 +15,15 @@ public final class EventBusSubscriber {
 
     private EventBusSubscriber() {
     }
-//
-//    @SubscribeEvent
-//    public static void onServerStarting(ServerStartingEvent event) {
-//        MinecraftServer server = event.getServer();
-//        Registry<LevelStem> registry = server.registryAccess().registryOrThrow(Registries.LEVEL_STEM);
-//        for (int index : MultiverseExistingData.getOrCreate(server).getExisting()) {
-//            ResourceLocation loc = DimensionHelper.getRegistryKey(index).location();
-//            if (!registry.containsKey(loc)) {
-//                Registry.register(registry, loc, DimensionHelper.createDimension(server, index));
-//            }
-//        }
-//    }
+
+    @SubscribeEvent
+    public static void onServerStarting(ServerStartingEvent event) {
+        Registry<NoiseGeneratorSettings> settings = event.getServer().registryAccess().registryOrThrow(Registries.NOISE_SETTINGS);
+        for (MultiverseShape shape : MultiverseShape.values()) {
+            for (MultiverseType type : MultiverseType.values()) {
+                ((IMultiverseNoiseGeneratorSettings) (Object) settings.get(shape.getNoiseSettingsKey(type))).setSettings(shape, type);
+            }
+        }
+    }
 
 }
