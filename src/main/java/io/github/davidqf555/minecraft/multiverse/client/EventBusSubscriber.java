c921bb26422323fe415665df0e0befddb359757f
DavidQF555 <davidfeng2003@gmail.com>
2023-07-24 22:28:45 -0700

    added fog effects

--- src/main/java/io/github/davidqf555/minecraft/multiverse/client/EventBusSubscriber.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/client/EventBusSubscriber.java
@@ -1,12 +1,15 @@
 package io.github.davidqf555.minecraft.multiverse.client;
 
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import io.github.davidqf555.minecraft.multiverse.client.effects.ColoredFogEffect;
 import io.github.davidqf555.minecraft.multiverse.client.render.MixedIllagerRenderer;
 import io.github.davidqf555.minecraft.multiverse.client.render.RiftTileEntityRenderer;
 import io.github.davidqf555.minecraft.multiverse.common.Multiverse;
 import io.github.davidqf555.minecraft.multiverse.common.entities.CollectorEntity;
 import io.github.davidqf555.minecraft.multiverse.registration.EntityRegistry;
 import io.github.davidqf555.minecraft.multiverse.registration.TileEntityRegistry;
+import io.github.davidqf555.minecraft.multiverse.registration.worldgen.DimensionTypeEffectsRegistry;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,6 +17,7 @@ import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterShadersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.io.IOException;
 
@@ -32,6 +36,13 @@ public final class EventBusSubscriber {
     @SubscribeEvent
     public static void onRegisterShaders(RegisterShadersEvent event) throws IOException {
         event.registerShader(new ShaderInstance(event.getResourceManager(), new ResourceLocation(Multiverse.MOD_ID, "rift"), DefaultVertexFormat.POSITION_COLOR), shader -> RiftTileEntityRenderer.SHADER = shader);
+    }
+
+    @SubscribeEvent
+    public static void onFMLClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            DimensionTypeEffectsRegistry.FOG.forEach((key, color) -> DimensionSpecialEffects.EFFECTS.put(key, new ColoredFogEffect(color)));
+        });
     }
 
 }
--- src/main/java/io/github/davidqf555/minecraft/multiverse/registration/worldgen/DimensionTypeEffectsRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/registration/worldgen/DimensionTypeEffectsRegistry.java
@@ -2,13 +2,26 @@ package io.github.davidqf555.minecraft.multiverse.registration.worldgen;
 
 import io.github.davidqf555.minecraft.multiverse.common.Multiverse;
 import io.github.davidqf555.minecraft.multiverse.common.worldgen.MultiverseEffectType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mod.EventBusSubscriber(modid = Multiverse.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class DimensionTypeEffectsRegistry {
+
+    public static final Map<ResourceLocation, Integer> FOG = new HashMap<>();
+
+    static {
+        for (DyeColor color : DyeColor.values()) {
+            FOG.put(new ResourceLocation(Multiverse.MOD_ID, color.getName() + "_fog"), color.getFireworkColor());
+        }
+    }
 
     private DimensionTypeEffectsRegistry() {
     }
@@ -16,9 +29,11 @@ public final class DimensionTypeEffectsRegistry {
     @SubscribeEvent
     public static void onFMLCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            MultiverseEffectType.register(new MultiverseEffectType(1, false, DimensionType.OVERWORLD_EFFECTS));
-            MultiverseEffectType.register(new MultiverseEffectType(1, false, DimensionType.NETHER_EFFECTS));
-            MultiverseEffectType.register(new MultiverseEffectType(1, true, DimensionType.END_EFFECTS));
+            MultiverseEffectType.register(new MultiverseEffectType(16, false, DimensionType.OVERWORLD_EFFECTS));
+            MultiverseEffectType.register(new MultiverseEffectType(4, true, DimensionType.END_EFFECTS));
+            for (ResourceLocation key : FOG.keySet()) {
+                MultiverseEffectType.register(new MultiverseEffectType(1, false, key));
+            }
         });
     }
 
