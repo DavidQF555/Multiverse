ad3c9246d35675bde70fe41baeb5f7b909e6d60c
DavidQF555 <davidfeng2003@gmail.com>
2023-07-30 20:04:34 -0700

    changed item structure

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/items/RiftCoreItem.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/items/RiftCoreItem.java
@@ -1,25 +1,42 @@
 package io.github.davidqf555.minecraft.multiverse.common.items;
 
 import io.github.davidqf555.minecraft.multiverse.common.blocks.RiftBlock;
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.features.RiftConfig;
+import io.github.davidqf555.minecraft.multiverse.registration.BlockRegistry;
+import io.github.davidqf555.minecraft.multiverse.registration.worldgen.FeatureRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.LevelEvent;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 
-public class RiftRemovalItem extends Item {
+import java.util.Optional;
+
+public class RiftCoreItem extends Item {
 
     private static final int MAX_RANGE = 50;
 
-    public RiftRemovalItem(Properties properties) {
+    public RiftCoreItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void onDestroyed(ItemEntity entity, DamageSource damageSource) {
+        if (!damageSource.isBypassInvul()) {
+            FeatureRegistry.RIFT.get().place(new FeaturePlaceContext<>(Optional.empty(), (ServerLevel) entity.level, ((ServerLevel) entity.level).getChunkSource().getGenerator(), entity.level.getRandom(), entity.blockPosition(), RiftConfig.of(Optional.empty(), BlockRegistry.RIFT.get().defaultBlockState().setValue(RiftBlock.TEMPORARY, false), false)));
+        }
     }
 
     @Override
     public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
         BlockPos pos = entity.blockPosition();
         if (entity.level.getBlockState(pos).getBlock() instanceof RiftBlock) {
+            entity.level.levelEvent(LevelEvent.ANIMATION_END_GATEWAY_SPAWN, pos, 0);
             removeConnected(entity, pos, 0);
             entity.discard();
             return true;
--- src/main/java/io/github/davidqf555/minecraft/multiverse/registration/ItemRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/registration/ItemRegistry.java
@@ -18,9 +18,8 @@ public final class ItemRegistry {
 
     public static final RegistryObject<RiftSwordItem> BOUNDLESS_BLADE = register("boundless_blade", () -> new RiftSwordItem(KaleiditeItemTier.INSTANCE, 2, -2.4f, new Item.Properties().rarity(Rarity.EPIC).tab(CreativeModeTab.TAB_COMBAT)));
     public static final RegistryObject<SpawnCollectorItem> UNIVERSAL_TREASURE = register("universal_treasure", () -> new SpawnCollectorItem(new Item.Properties().rarity(Rarity.RARE).tab(CreativeModeTab.TAB_MISC).stacksTo(1).fireResistant(), 300));
-    public static final RegistryObject<SpawnRiftItem> UNSTABLE_CATALYST = register("unstable_catalyst", () -> new SpawnRiftItem(new Item.Properties().rarity(Rarity.UNCOMMON).tab(CreativeModeTab.TAB_MISC), 200));
     public static final RegistryObject<RiftDeathItem> TOTEM_OF_ESCAPE = register("totem_of_escape", () -> new RiftDeathItem(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC), 5));
-    public static final RegistryObject<RiftRemovalItem> KALEIDITE_NEUTRALIZER = register("kaleidite_neutralizer", () -> new RiftRemovalItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<RiftCoreItem> KALEIDITE_CORE = register("kaleidite_core", () -> new RiftCoreItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC).rarity(Rarity.UNCOMMON)));
     public static final RegistryObject<Item> KALEIDITE = register("kaleidite", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
     public static final RegistryObject<BlockItem> KALEIDIUM_ORE = register("kaleidite_ore", () -> new BlockItem(BlockRegistry.KALEIDITE_ORE.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
