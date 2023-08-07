9991a642660e1387f0269cb70273619ef8e37b10
DavidQF555 <davidfeng2003@gmail.com>
2023-08-07 05:39:00 -0700

    improved solid placement

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/features/placement/SolidPlacement.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/features/placement/SolidPlacement.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.davidqf555.minecraft.multiverse.registration.worldgen.PlacementRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.placement.PlacementContext;
@@ -18,7 +19,7 @@ public class SolidPlacement extends PlacementModifier {
 
     public static final Codec<SolidPlacement> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             Direction.CODEC.fieldOf("direction").forGetter(placement -> placement.dir),
-            Codec.INT.fieldOf("steps").forGetter(placement -> placement.steps)
+            ExtraCodecs.POSITIVE_INT.fieldOf("steps").forGetter(placement -> placement.steps)
     ).apply(inst, SolidPlacement::of));
     private final Direction dir;
     private final int steps;
@@ -38,8 +39,7 @@ public class SolidPlacement extends PlacementModifier {
     public Stream<BlockPos> getPositions(PlacementContext context, Random random, BlockPos pos) {
         WorldGenLevel level = context.getLevel();
         BlockPos.MutableBlockPos mutable = pos.mutable();
-        int step = 0;
-        do {
+        for (int step = 0; step < steps; step++) {
             if (level.isOutsideBuildHeight(mutable) || !BlockPredicate.ONLY_IN_AIR_OR_WATER_PREDICATE.test(level, mutable)) {
                 break;
             }
@@ -47,8 +47,7 @@ public class SolidPlacement extends PlacementModifier {
                 return Stream.of(mutable);
             }
             mutable.move(dir);
-            step++;
-        } while (step < steps);
+        }
         return Stream.empty();
     }
 
