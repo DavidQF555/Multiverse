91ad88f6d155ba5457f9bd3d164db7f43f139839
DavidQF555 <davidfeng2003@gmail.com>
2023-09-05 19:54:57 -0500

    fixed traveler spawns

--- src/main/java/io/github/davidqf555/minecraft/multiverse/registration/EntityRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/registration/EntityRegistry.java
@@ -42,7 +42,7 @@ public final class EntityRegistry {
 
     @SubscribeEvent
     public static void onRegisterSpawnPlacement(SpawnPlacementRegisterEvent event) {
-        event.register(TRAVELER.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TravelerEntity::canSpawn, SpawnPlacementRegisterEvent.Operation.AND);
+        event.register(TRAVELER.get(), SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TravelerEntity::canSpawn, SpawnPlacementRegisterEvent.Operation.AND);
         event.register(DOPPELGANGER.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, DoppelgangerEntity::canSpawn, SpawnPlacementRegisterEvent.Operation.AND);
     }
 
