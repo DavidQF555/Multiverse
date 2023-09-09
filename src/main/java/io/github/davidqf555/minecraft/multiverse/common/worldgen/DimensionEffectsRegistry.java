8118d5bdd095d7ca4ad60a61b0e1f14ecc5880cd
DavidQF555 <davidfeng2003@gmail.com>
2023-09-09 16:16:08 -0500

    changed effect weights

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/DimensionEffectsRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/DimensionEffectsRegistry.java
@@ -28,8 +28,8 @@ public final class DimensionEffectsRegistry {
     @SubscribeEvent
     public static void onFMLCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            MultiverseEffectType.register(new MultiverseEffectType(16, false, DimensionType.OVERWORLD_EFFECTS));
-            MultiverseEffectType.register(new MultiverseEffectType(4, true, DimensionType.END_EFFECTS));
+            MultiverseEffectType.register(new MultiverseEffectType(32, false, DimensionType.OVERWORLD_EFFECTS));
+            MultiverseEffectType.register(new MultiverseEffectType(8, true, DimensionType.END_EFFECTS));
             for (ResourceLocation key : FOG.keySet()) {
                 MultiverseEffectType.register(new MultiverseEffectType(1, false, key));
             }
