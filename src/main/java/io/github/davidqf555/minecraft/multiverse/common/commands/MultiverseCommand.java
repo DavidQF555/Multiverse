fdc726e2a7068db1147a7134731740604da9d8cc
DavidQF555 <davidfeng2003@gmail.com>
2023-11-13 11:11:46 -0600

    fixed command

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/commands/MultiverseCommand.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/commands/MultiverseCommand.java
@@ -28,6 +28,7 @@ public final class MultiverseCommand {
 
     private static final Component NO_DIMENSIONS = Component.translatable(Util.makeDescriptionId("command", new ResourceLocation(Multiverse.MOD_ID, "multiverse.no_dimensions")));
     private static final Component INDICES_HEADER = Component.translatable(Util.makeDescriptionId("command", new ResourceLocation(Multiverse.MOD_ID, "multiverse.indices_header")));
+    private static final Component OUT_OF_BOUNDS = Component.translatable(Util.makeDescriptionId("command", new ResourceLocation(Multiverse.MOD_ID, "multiverse.out_of_bounds")));
     private static final String INDEX = Util.makeDescriptionId("command", new ResourceLocation(Multiverse.MOD_ID, "multiverse.index"));
 
     private MultiverseCommand() {
@@ -40,7 +41,7 @@ public final class MultiverseCommand {
                         .executes(context -> list(context.getSource()))
                 )
                 .then(Commands.literal("teleport")
-                        .then(Commands.argument("index", IntegerArgumentType.integer(0, ServerConfigs.INSTANCE.maxDimensions.get()))
+                        .then(Commands.argument("index", IntegerArgumentType.integer(0))
                                 .executes(context -> teleport(context.getSource(), IntegerArgumentType.getInteger(context, "index")))
                         )
                 )
@@ -65,6 +66,10 @@ public final class MultiverseCommand {
     }
 
     private static int teleport(CommandSourceStack stack, int index) throws CommandSyntaxException {
+        if (index > ServerConfigs.INSTANCE.maxDimensions.get()) {
+            stack.sendFailure(OUT_OF_BOUNDS);
+            return 0;
+        }
         Entity entity = stack.getEntityOrException();
         ServerLevel world = DimensionHelper.getOrCreateWorld(stack.getServer(), index);
         Vec3 pos = DimensionHelper.translate(entity.position(), entity.level.dimensionType(), world.dimensionType(), true);
