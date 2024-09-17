3188626249cc241d5a55181a7d11be4f544e72a8
DavidQF555 <davidfeng2003@gmail.com>
2024-09-17 07:17:25 -0500

    improved biome type counts

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/biomes/BiomeConfig.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/biomes/BiomeConfig.java
@@ -1,28 +1,89 @@
 package io.github.davidqf555.minecraft.multiverse.common.worldgen.biomes;
 
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.MultiverseType;
+import io.github.davidqf555.minecraft.multiverse.common.worldgen.providers.biomes.chunk_gen.sea_level.IntRange;
 import io.github.davidqf555.minecraft.multiverse.registration.custom.BiomeConfigRegistry;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.RegistryFileCodec;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.RandomSource;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class BiomeConfig extends ForgeRegistryEntry<BiomeConfig> {
 
     public static final Codec<BiomeConfig> DIRECT_CODEC = RecordCodecBuilder.create(inst -> inst.group(
-            BiomeType.CODEC.listOf().fieldOf("types").forGetter(BiomeConfig::getTypes)
+            BiomeType.CODEC.listOf().fieldOf("types").forGetter(BiomeConfig::getTypes),
+            IntRange.CODEC.optionalFieldOf("count", IntRange.of(1, 1)).forGetter(BiomeConfig::getCount)
     ).apply(inst, BiomeConfig::new));
     public static final Codec<Holder<BiomeConfig>> CODEC = RegistryFileCodec.create(BiomeConfigRegistry.LOCATION, DIRECT_CODEC);
     private final List<BiomeType> types;
+    private final IntRange count;
 
-    public BiomeConfig(List<BiomeType> types) {
+    public BiomeConfig(List<BiomeType> types, IntRange count) {
         this.types = types;
+        this.count = count;
     }
 
     public List<BiomeType> getTypes() {
         return types;
+    }
+
+    private static BiomeType selectRandom(RandomSource random, List<BiomeType> types) {
+        int total = types.stream().mapToInt(BiomeType::getWeight).sum();
+        int selected = random.nextInt(total);
+        for (BiomeType type : types) {
+            total -= type.getWeight();
+            if (total <= selected) {
+                return type;
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    public IntRange getCount() {
+        return count;
+    }
+
+    public Pair<MultiverseType, Set<ResourceKey<Biome>>> selectRandom(Registry<Biome> registry, RandomSource rand) {
+        Set<MultiverseType> biomesTypes = EnumSet.allOf(MultiverseType.class);
+        Predicate<ResourceKey<Biome>> valid = key -> biomesTypes.stream().anyMatch(type -> type.is(key));
+        List<BiomeType> types = getTypes().stream().filter(type -> type.getBiomes(registry).stream().anyMatch(valid)).collect(Collectors.toList());
+        Set<ResourceKey<Biome>> biomes = new HashSet<>();
+        if (types.isEmpty()) {
+            biomes.add(Biomes.THE_VOID);
+        } else {
+            BiomeType type = selectRandom(rand, types);
+            types.remove(type);
+            biomes.addAll(type.getBiomes(registry));
+        }
+        int count = Math.min(types.size(), this.count.getRandom(rand));
+        for (int i = 0; i < count; i++) {
+            BiomeType type = selectRandom(rand, types);
+            types.remove(type);
+            biomes.addAll(type.getBiomes(registry));
+        }
+        biomes.removeIf(valid.negate());
+        Map<MultiverseType, Integer> counts = new EnumMap<>(MultiverseType.class);
+        for (ResourceKey<Biome> biome : biomes) {
+            for (MultiverseType type : biomesTypes) {
+                if (type.is(biome)) {
+                    counts.compute(type, (t, current) -> current == null ? 1 : current + 1);
+                }
+            }
+        }
+        MultiverseType type = counts.keySet().stream().max((i, j) -> counts.get(j) - counts.get(i)).orElseThrow();
+        biomes.removeIf(key -> !type.is(key));
+        return Pair.of(type, biomes);
     }
 
 }
--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/providers/biomes/BiomeConfigDimensionProvider.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/worldgen/providers/biomes/BiomeConfigDimensionProvider.java
@@ -1,89 +1,42 @@
 package io.github.davidqf555.minecraft.multiverse.common.worldgen.providers.biomes;
 
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.davidqf555.minecraft.multiverse.common.worldgen.MultiverseType;
 import io.github.davidqf555.minecraft.multiverse.common.worldgen.biomes.BiomeConfig;
-import io.github.davidqf555.minecraft.multiverse.common.worldgen.biomes.BiomeType;
 import io.github.davidqf555.minecraft.multiverse.common.worldgen.providers.DimensionProvider;
 import io.github.davidqf555.minecraft.multiverse.common.worldgen.providers.DimensionProviderType;
-import io.github.davidqf555.minecraft.multiverse.common.worldgen.providers.biomes.chunk_gen.sea_level.IntRange;
 import io.github.davidqf555.minecraft.multiverse.registration.custom.DimensionProviderTypeRegistry;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.levelgen.RandomSource;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class BiomeConfigDimensionProvider implements DimensionProvider {
 
     public static final Codec<BiomeConfigDimensionProvider> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             BiomeConfig.CODEC.fieldOf("biomes").forGetter(val -> val.config),
-            BiomeDimensionProvider.CODEC.fieldOf("dimension").forGetter(val -> val.provider),
-            IntRange.CODEC.optionalFieldOf("count", IntRange.of(1, 1)).forGetter(val -> val.count)
+            BiomeDimensionProvider.CODEC.fieldOf("dimension").forGetter(val -> val.provider)
     ).apply(inst, BiomeConfigDimensionProvider::new));
     private final Holder<BiomeConfig> config;
     private final BiomeDimensionProvider provider;
-    private final IntRange count;
 
-    public BiomeConfigDimensionProvider(Holder<BiomeConfig> config, BiomeDimensionProvider provider, IntRange count) {
+    public BiomeConfigDimensionProvider(Holder<BiomeConfig> config, BiomeDimensionProvider provider) {
         this.config = config;
         this.provider = provider;
-        this.count = count;
-    }
-
-    private static BiomeType selectRandom(RandomSource random, List<BiomeType> types) {
-        int total = types.stream().mapToInt(BiomeType::getWeight).sum();
-        int selected = random.nextInt(total);
-        for (BiomeType type : types) {
-            total -= type.getWeight();
-            if (total <= selected) {
-                return type;
-            }
-        }
-        throw new RuntimeException();
     }
 
     @Override
     public LevelStem createDimension(RegistryAccess access, long seed, RandomSource random) {
         BiomeConfig config = this.config.value();
-        Registry<Biome> registry = access.registryOrThrow(Registry.BIOME_REGISTRY);
-        Set<MultiverseType> biomesTypes = EnumSet.allOf(MultiverseType.class);
-        Predicate<ResourceKey<Biome>> valid = key -> biomesTypes.stream().anyMatch(type -> type.is(key));
-        List<BiomeType> types = config.getTypes().stream().filter(type -> type.getBiomes(registry).stream().anyMatch(valid)).collect(Collectors.toList());
-        Set<ResourceKey<Biome>> biomes = new HashSet<>();
-        if (types.isEmpty()) {
-            biomes.add(Biomes.THE_VOID);
-        } else {
-            BiomeType type = selectRandom(random, types);
-            types.remove(type);
-            biomes.addAll(type.getBiomes(registry));
-        }
-        int count = Math.min(types.size(), this.count.getRandom(random));
-        for (int i = 0; i < count; i++) {
-            BiomeType type = selectRandom(random, types);
-            types.remove(type);
-            biomes.addAll(type.getBiomes(registry));
-        }
-        biomes.removeIf(valid.negate());
-        Map<MultiverseType, Integer> counts = new EnumMap<>(MultiverseType.class);
-        for (ResourceKey<Biome> biome : biomes) {
-            for (MultiverseType type : biomesTypes) {
-                if (type.is(biome)) {
-                    counts.compute(type, (t, current) -> current == null ? 1 : current + 1);
-                }
-            }
-        }
-        MultiverseType type = counts.keySet().stream().max((i, j) -> counts.get(j) - counts.get(i)).orElseThrow();
-        biomes.removeIf(key -> !type.is(key));
-        return provider.provide(access, seed, random, type, biomes);
+        Pair<MultiverseType, Set<ResourceKey<Biome>>> biomes = config.selectRandom(access.registryOrThrow(Registry.BIOME_REGISTRY), random);
+        return provider.provide(access, seed, random, biomes.getFirst(), biomes.getSecond());
     }
 
     @Override
