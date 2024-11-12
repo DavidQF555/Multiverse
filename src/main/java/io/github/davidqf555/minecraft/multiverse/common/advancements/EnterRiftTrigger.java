18d2020af7e4b9e3aa9d2846b180672771916c2d
DavidQF555 <davidfeng2003@gmail.com>
2024-11-12 06:13:04 -0600

    fix trigger again

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/advancements/EnterRiftTrigger.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/advancements/EnterRiftTrigger.java
@@ -1,9 +1,9 @@
 package io.github.davidqf555.minecraft.multiverse.common.advancements;
 
-import com.google.gson.JsonObject;
-import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.DeserializationContext;
+import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -11,22 +11,23 @@ import java.util.Optional;
 
 public class EnterRiftTrigger extends SimpleCriterionTrigger<EnterRiftTrigger.Instance> {
 
-    public static final EnterRiftTrigger INSTANCE = new EnterRiftTrigger();
+    public static final Codec<Instance> CODEC = RecordCodecBuilder.create(inst -> inst.group(
+                    EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(Instance::player)
+            )
+            .apply(inst, Instance::new));
 
-    @Override
-    protected Instance createInstance(JsonObject pJson, Optional<ContextAwarePredicate> pPlayer, DeserializationContext pContext) {
-        return new Instance(pPlayer);
-    }
+    public static final EnterRiftTrigger INSTANCE = new EnterRiftTrigger();
 
     public void trigger(ServerPlayer pPlayer) {
         super.trigger(pPlayer, val -> true);
     }
 
-    public static class Instance extends AbstractCriterionTriggerInstance {
-
-        public Instance(Optional<ContextAwarePredicate> pPlayer) {
-            super(pPlayer);
-        }
-
+    @Override
+    public Codec<Instance> codec() {
+        return CODEC;
     }
+
+    public record Instance(Optional<ContextAwarePredicate> player) implements SimpleInstance {
+    }
+
 }
