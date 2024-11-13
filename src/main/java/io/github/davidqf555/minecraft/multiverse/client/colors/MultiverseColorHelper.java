82a7e676f4a147ba8f79979582ab2a0f0d42cd87
DavidQF555 <davidfeng2003@gmail.com>
2024-11-12 20:47:23 -0600

    improved possible rift colors

--- src/main/java/io/github/davidqf555/minecraft/multiverse/client/colors/MultiverseColorHelper.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/client/colors/MultiverseColorHelper.java
@@ -4,7 +4,6 @@ import io.github.davidqf555.minecraft.multiverse.common.worldgen.DimensionHelper
 import net.minecraft.util.FastColor;
 import net.minecraft.world.level.Level;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public final class MultiverseColorHelper {
@@ -16,7 +15,7 @@ public final class MultiverseColorHelper {
 
     private static int getColor(Random rand) {
         int[] color = new int[]{rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)};
-        applyTransformations(color, rand);
+        shift(color, rand);
         return FastColor.ARGB32.color(0xFF, color[0], color[1], color[2]);
     }
 
@@ -33,26 +32,9 @@ public final class MultiverseColorHelper {
         return getColor(RANDOM);
     }
 
-    private static void applyTransformations(int[] color, Random rand) {
-        shift(color, rand);
-        maximizeSaturation(color);
-    }
-
     private static void shift(int[] color, Random rand) {
         int i = rand.nextInt(color.length);
-        color[i] = color[i] < 0x80 ? 0xFF : 0x00;
-    }
-
-    private static void maximizeSaturation(int[] color) {
-        Arrays.stream(color).max().ifPresent(max -> {
-            if (max == 0x00) {
-                Arrays.fill(color, 0xFF);
-            } else {
-                for (int i = 0; i < color.length; i++) {
-                    color[i] = color[i] * 0xFF / max;
-                }
-            }
-        });
+        color[i] = color[i] < 0x80 ? 0x00 : 0xFF;
     }
 
 }
