9fd3702bb80e13d9118c8366e59eb8f74de442b8
DavidQF555 <davidfeng2003@gmail.com>
2025-01-14 18:05:04 -0800

    improved rift placement

--- src/main/java/io/github/davidqf555/minecraft/multiverse/common/util/WarpTeleporter.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/common/util/WarpTeleporter.java
@@ -71,7 +71,7 @@ public class WarpTeleporter implements ITeleporter {
         Vec3 to = Vec3.atBottomCenterOf(clamped);
         AABB box = AABB.ofSize(to.add(0, entity.getBbHeight() / 2, 0), entity.getBbWidth(), entity.getBbHeight(), entity.getBbWidth());
         BlockPos.betweenClosedStream(box)
-                .filter(pos -> !destWorld.isOutsideBuildHeight(pos) && RiftPlacementHelper.canDestroy(destWorld, pos, destWorld.getBlockState(pos)))
+                .filter(pos -> !destWorld.isOutsideBuildHeight(pos) && RiftPlacementHelper.ReplacementType.DESTROY.canReplace(destWorld, pos, destWorld.getBlockState(pos)))
                 .forEach(pos -> destWorld.destroyBlock(pos, true));
         return new PortalInfo(to, Vec3.ZERO, entity.getYRot(), entity.getXRot());
     }
