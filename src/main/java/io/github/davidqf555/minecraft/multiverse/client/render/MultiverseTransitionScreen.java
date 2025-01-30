210e0b950e6c3bd58b15608f92e98f2e8e6a7f97
DavidQF555 <davidfeng2003@gmail.com>
2025-01-30 14:14:01 -0800

    fixed transition screen

--- src/main/java/io/github/davidqf555/minecraft/multiverse/client/render/MultiverseTransitionScreen.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/client/render/MultiverseTransitionScreen.java
@@ -1,9 +1,11 @@
 package io.github.davidqf555.minecraft.multiverse.client.render;
 
+import io.github.davidqf555.minecraft.multiverse.client.ClientConfigs;
 import io.github.davidqf555.minecraft.multiverse.client.ShaderHelper;
 import io.github.davidqf555.minecraft.multiverse.client.colors.MultiverseColorHelper;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.ReceivingLevelScreen;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 
 import java.util.function.BooleanSupplier;
@@ -12,14 +14,14 @@ public class MultiverseTransitionScreen extends ReceivingLevelScreen {
 
     private final int color;
 
-    public MultiverseTransitionScreen(BooleanSupplier levelReceived, Reason reason, Level dim) {
+    public MultiverseTransitionScreen(BooleanSupplier levelReceived, Reason reason, ResourceKey<Level> dim) {
         super(levelReceived, reason);
-        color = MultiverseColorHelper.getColors(dim.dimension(), 1)[0];
+        color = MultiverseColorHelper.getColors(dim, 1)[0];
     }
 
     @Override
     public void renderBackground(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        guiGraphics.fill(ShaderHelper.RIFT, 0, 0, width, height, color);
+        guiGraphics.fill(ClientConfigs.INSTANCE.vanillaOnly.get() ? ShaderHelper.RIFT_VANILLA : ShaderHelper.RIFT, 0, 0, width, height, color);
     }
 
 }
--- src/main/java/io/github/davidqf555/minecraft/multiverse/mixin/MixinDimensionTransitionScreenManager.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/mixin/MixinDimensionTransitionScreenManager.java
@@ -2,6 +2,7 @@ package io.github.davidqf555.minecraft.multiverse.mixin;
 
 import io.github.davidqf555.minecraft.multiverse.client.render.MultiverseTransitionScreen;
 import io.github.davidqf555.minecraft.multiverse.common.world.DimensionHelper;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.client.DimensionTransitionScreenManager;
 import org.jetbrains.annotations.Nullable;
@@ -13,12 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = DimensionTransitionScreenManager.class)
 public class MixinDimensionTransitionScreenManager {
 
-    @Inject(method = "getScreenFromLevel", at = @At("HEAD"), cancellable = true)
-    private static void getScreenFromLevel(@Nullable Level to, @Nullable Level from, CallbackInfoReturnable<DimensionTransitionScreenManager.ReceivingLevelScreenFactory> callback) {
-        if (to != null) {
-            int index = DimensionHelper.getIndex(to.dimension()).orElse(-1);
-            if (index > 0 || from != null && index == 0 && DimensionHelper.getIndex(from.dimension()).filter(i -> i > 0).isPresent()) {
-                callback.setReturnValue(((supplier, reason) -> new MultiverseTransitionScreen(supplier, reason, to)));
+    @Inject(method = "getScreen", at = @At("HEAD"), cancellable = true)
+    private static void getScreen(@Nullable ResourceKey<Level> toDimension, @Nullable ResourceKey<Level> fromDimension, CallbackInfoReturnable<DimensionTransitionScreenManager.ReceivingLevelScreenFactory> callback) {
+        if (toDimension != null) {
+            int index = DimensionHelper.getIndex(toDimension).orElse(-1);
+            if (index > 0 || fromDimension != null && index == 0 && DimensionHelper.getIndex(fromDimension).filter(i -> i > 0).isPresent()) {
+                callback.setReturnValue(((supplier, reason) -> new MultiverseTransitionScreen(supplier, reason, toDimension)));
             }
         }
     }
